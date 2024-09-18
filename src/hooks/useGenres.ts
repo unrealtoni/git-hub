ce07bb8de6a5b264639bf726055f717f563e80bd
Toni Georgiev <unrealtoni@gmail.com>
2024-09-18 20:04:43 +0200

    Fetch the genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchedGenresRespponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get<FetchedGenresRespponse>("/genres", { signal: controller.signal })
+      .then((res) => setGenres(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        return () => controller.abort();
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
