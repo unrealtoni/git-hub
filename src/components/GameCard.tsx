67717fde4f262edbd176898be72d15fec827bdb5
Toni Georgiev <unrealtoni@gmail.com>
2024-09-18 18:31:25 +0200

    Refactor: Remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,25 +3,28 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCropedImageUrl from "../services/image-url";
+import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width={"200px"} borderRadius={10} overflow={"hidden"}>
-      <Image src={getCropedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
-          <PlatformIconList
-            key={game.id}
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Image src={getCropedImageUrl(game.background_image)} />
+        <CardBody>
+          <Heading fontSize={"2xl"}>{game.name}</Heading>
+          <HStack justifyContent={"space-between"}>
+            <PlatformIconList
+              key={game.id}
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,13 +1,16 @@
 import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import GameCardContainer from "./GameCardContainer";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width={"200px"} borderRadius={10} overflow={"hidden"}>
-      <Skeleton height={"200px"} />
-      <CardBody>
-        <SkeletonText />
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Skeleton height={"200px"} />
+        <CardBody>
+          <SkeletonText />
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
